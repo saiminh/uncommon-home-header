2c34e00649e28ae9211375d9bf435687da8d9cde
saiminh <saiminh@gmail.com>
2023-05-23 17:31:26 +0200

    optimise css

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => '5ed5609c24e98ab52e10');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-element', 'wp-i18n'), 'version' => 'd451e564b5159832896d');
