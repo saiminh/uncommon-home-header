4f1ae6a7532fea7e69bc5f0cc0942a49822b4690
saiminh <saiminh@gmail.com>
2023-05-30 18:55:21 +0100

    block meta

--- uncommon-home-header.php
+++ uncommon-home-header.php
@@ -1,11 +1,11 @@
 <?php
 /**
  * Plugin Name:       Uncommon Home Header
- * Description:       Example block scaffolded with Create Block tool.
+ * Description:       The Uncommon Homepage Header, requires Uncommon Home Header Slide
  * Requires at least: 6.1
  * Requires PHP:      7.0
- * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Version:           1.0
+ * Author:            Simon Flöter
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       uncommon-home-header
